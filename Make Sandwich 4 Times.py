553ba350173e4a1b673dd9a2cdf203df70655f18
abhishekcode45 <abhishekgadhe45@gmail.com>
2025-10-29 00:33:55 +0530

    Create Make Sandwich 4 Times.py

---
+++ Make Sandwich 4 Times.py
@@ -0,0 +1,11 @@
+def make_sandwich():
+    print("Get two slices of bread")
+    print("Add peanut butter")
+    print("Add jelly")
+    print("Put the slices together")
+    print("Serve the sandwich\n")
+
+make_sandwich()
+make_sandwich()
+make_sandwich()
+make_sandwich()
